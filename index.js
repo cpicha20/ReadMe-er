b5168150d03d08cba34afe7e80ba630bd37945d4
cpicha20 <cpichardowps@gmail.com>
2023-10-23 22:58:58 -0400

    Created printReadMe function

--- index.js
+++ index.js
@@ -15,6 +15,53 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const printHTML =  ({title, description , installation , usage, license, contributing, test, ghUsername, questions}) => fs.writeFile('index.html',`# ${title}
+
+![license](https://img.shields.io/badge/${license})
+
+
+## Description
+
+${description}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+this project was created with the ${license} license.
+
+## How to Contribute
+
+${contributing}
+
+## Tests
+
+${test}
+
+##Questions
+
+${questions}
+[${ghUsername}](https://github.com/${ghUsername})
+
+
+
+
+` , (err) =>
+  err ? console.error(err) : console.log('Success!'));
 
 
 
@@ -23,29 +70,69 @@ inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'userName',
+      message: 'What is the name of the project?',
+      name: 'title',
     },
     {
       type: 'input',
-      message: 'Where are you from?',
-      name: 'userLocation',
+      message: 'Can you write a description for your project?',
+      name: 'description',
     },
     {
       type: 'input',
-      message: 'Tell me about yourself',
-      name: 'bio',
+      message: 'Can you write installation ins',
+      name: 'installation',
     },
     {
       type: 'input',
       message: 'What is your linkedin url?',
-      name: 'liURL',
+      name: 'usage',
     },
     {
       type: 'input',
       message: 'What is your github url?',
-      name: 'ghURL',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'What is your github url?',
+      name: 'test',
+    },
+    {
+      type: 'list',
+      message: 'What is your github url?',
+      choices:["MIT License",
+      "GNU General Public License v3.0",
+      "Apache License 2.0",
+      "BSD 2-Clause \"Simplified\" License",
+      "BSD 3-Clause \"New\" or \"Revised\" License",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public License v2.1",
+      "Mozilla Public License 2.0",
+      "Common Development and Distribution License 1.0",
+      "Eclipse Public License 2.0",
+      "The Unlicense",
+      "GNU General Public License v2.0 or later",
+      "GNU Lesser General Public License v3.0",
+      "Do What The F*ck You Want To Public License",
+      "Microsoft Public License",
+      "GNU Lesser General Public License v2.0 or later",
+      "SIL Open Font License 1.1",
+      "zlib License",] ,
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'What is your github url?',
+      name: 'questions',
+    },
+    {
+      type: 'input',
+      message: 'What is your github url?',
+      name: 'ghUsername',
     },
   ])
   .then((response) => printHTML(response));
 
+  title, description , installation , usage, license, contributing, test, ghUsername, questions
